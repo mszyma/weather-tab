74ee43d3c1e1ecb4a924258053bbe175c4063ebb
Mariusz Szyma <m.szyma92@gmail.com>
2017-03-12 17:52:25 +0100

    OpenWeather icon added

--- js/main.js
+++ js/main.js
@@ -1,14 +1,34 @@
+$.getJSON('http://ipinfo.io/json', function(data) {
+    console.log(data);
+    $("#cityName").html(data.city);
+    var loc = data.loc.split(',');
+    var coords = {
+        latitude: loc[0],
+        longitude: loc[1]
+    };
+    console.log(coords.latitude, coords.longitude);
 
-var url = "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=2d34a8421468ffb44f89aeceaf08ac07";
 
-$.getJSON('http://ipinfo.io/json', function(data){
-  console.log(data);
+    var params = [
+      "http://api.openweathermap.org/data/2.5/weather?",
+      coords.latitude,
+      coords.longitude,
+      "&APPID=2d34a8421468ffb44f89aeceaf08ac07",
+      "&units=metric",
+    ];
+
+    var url = params[0] + "lat=" + params[1] + "&lon="+params[2] + params[3] + params[4];
+    console.log(url);
+
+
+
+    $(document).ready(function() {
+        $.getJSON(url, function(json) {
+            console.log(json);
+              var icon = "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png";
+              console.log(icon);
+            $("#weatherIconOpenWeather").html("<img src='" + icon + " '</img>");
+            $("#temp").html(json.main.temp);
+        });
+    });
 });
-
-/*
-$(document).ready(function() {
-  $.getJSON(url, function(json) {
-  console.log(json);
-  });
-});
-*/
