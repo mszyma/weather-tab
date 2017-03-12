d32b383f43d132f3fc69f5963462e50c97b29157
Mariusz Szyma <m.szyma92@gmail.com>
2017-03-12 18:35:38 +0100

    Weather types + 2 new weather types

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Getting location
 $.getJSON('http://ipinfo.io/json', function(data) {
     console.log(data);
     $("#cityName").html(data.city);
@@ -8,27 +9,48 @@ $.getJSON('http://ipinfo.io/json', function(data) {
     };
     console.log(coords.latitude, coords.longitude);
 
-
+//Getting weather
     var params = [
-      "http://api.openweathermap.org/data/2.5/weather?",
-      coords.latitude,
-      coords.longitude,
-      "&APPID=2d34a8421468ffb44f89aeceaf08ac07",
-      "&units=metric",
+        "http://api.openweathermap.org/data/2.5/weather?",
+        coords.latitude,
+        coords.longitude,
+        "&APPID=2d34a8421468ffb44f89aeceaf08ac07",
+        "&units=metric",
     ];
 
-    var url = params[0] + "lat=" + params[1] + "&lon="+params[2] + params[3] + params[4];
+    var url = params[0] + "lat=" + params[1] + "&lon=" + params[2] + params[3] + params[4];
     console.log(url);
-
-
 
     $(document).ready(function() {
         $.getJSON(url, function(json) {
             console.log(json);
-              var icon = "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png";
-              console.log(icon);
-            $("#weatherIconOpenWeather").html("<img src='" + icon + " '</img>");
-            $("#temp").html(json.main.temp);
+            var temp = json.main.temp;
+            $("#temp").html(temp); // Adding temperature
+
+            var weatherResult = json.weather[0].main;
+            switch (weatherResult) {
+                case "Thunderstorm":
+                    $("#weatherIconOpenWeather").html("<img src='img/thunder.png' class='img-responsive' alt='thunder'>");
+                    break;
+                case "Drizzle":
+                    $("#weatherIconOpenWeather").html("<img src='img/lightrain.png' class='img-responsive' alt='lightrain'>");
+                    break;
+                case "Rain":
+                    $("#weatherIconOpenWeather").html("<img src='img/rain.png' class='img-responsive' alt='rain'>");
+                    break;
+                case "Snow":
+                    $("#weatherIconOpenWeather").html("<img src='img/snow.png' class='img-responsive' alt='snow'>");
+                    break;
+                case "Atmosphere":
+                    $("#weatherIconOpenWeather").html("<img src='img/mist.png' class='img-responsive' alt='mist'>");
+                    break;
+                case "Clear":
+                    $("#weatherIconOpenWeather").html("<img src='img/sun.png' class='img-responsive' alt='sun'>");
+                    break;
+                case "Clouds":
+                    $("#weatherIconOpenWeather").html("<img src='img/cloudy.png' class='img-responsive' alt='cloudy'>");
+                    break;
+            }
         });
     });
 });
